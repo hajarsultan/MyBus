26ffbb0ce09f3b62629e341d09c0611ac7cdccc3
hajarsultan <hajarsultan50@gmail.com>
2016-06-15 01:08:30 +0200

    test

--- app/src/main/java/net/hajar/mybus/CompanyActivity.java
+++ app/src/main/java/net/hajar/mybus/CompanyActivity.java
@@ -9,5 +9,6 @@ public class CompanyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //jhilllllllllllll
     }
 }
